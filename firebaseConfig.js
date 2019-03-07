f90b7607c61dc93381c1acc24e39472c058414af
tommyno <tommy@netliferesearch.com>
2019-03-07 16:47:43 +0200

    Connect MeetupCreate with Firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,51 +1,47 @@
-import * as firebase from "firebase";
-import uuid from "uuid/v4";
+import * as firebase from 'firebase';
+import uuid from 'uuid/v4';
 
 var config = {
-  apiKey: "AIzaSyBvVYdjcVSyJ3_HdA54XWTjZGqmKbmj8tw",
-  authDomain: "yapp-70773.firebaseapp.com",
-  databaseURL: "https://yapp-70773.firebaseio.com",
-  projectId: "yapp-70773",
-  storageBucket: "yapp-70773.appspot.com",
-  messagingSenderId: "128914629238"
+  apiKey: 'AIzaSyBvVYdjcVSyJ3_HdA54XWTjZGqmKbmj8tw',
+  authDomain: 'yapp-70773.firebaseapp.com',
+  databaseURL: 'https://yapp-70773.firebaseio.com',
+  projectId: 'yapp-70773',
+  storageBucket: 'yapp-70773.appspot.com',
+  messagingSenderId: '128914629238',
 };
 firebase.initializeApp(config);
 export default {
-  postName: function(data) {
-    console.log("POST ", data);
-    const { userId = "", eventId = "" } = data;
-    console.log(name);
+  postEvent: function(eventData) {
+    const eventId = uuid();
+    console.log('firebase eventdata', eventData);
     firebase
       .database()
-      .ref("events/" + eventId)
-      .set({
-        username: name,
-        name: name
-      });
+      .ref('events/' + eventId)
+      .set(eventData);
+    return eventId;
   },
   getName: function(name) {
-    console.log("GET NAME ", name);
+    console.log('GET NAME ', name);
     return firebase
       .database()
-      .ref("users/" + name)
-      .once("value")
+      .ref('users/' + name)
+      .once('value')
       .then(function(snapshot) {
-        var username =
-          (snapshot.val() && snapshot.val().username) || "Anonymous";
+        var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
         console.log(snapshot.val());
         return username;
       });
   },
   getAll: function() {
-    console.log("GET ALL");
+    console.log('GET ALL');
     return firebase
       .database()
-      .ref("users/")
-      .once("value")
+      .ref('users/')
+      .once('value')
       .then(function(snapshot) {
         // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
         // console.log(snapshot.val());
         return snapshot.val();
       });
-  }
+  },
 };
--- src/screens/MeetupCreate.js
+++ src/screens/MeetupCreate.js
@@ -1,32 +1,54 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { Input } from "react-native-elements";
-import firebase from "../../firebaseConfig";
-
-const createEvent = () => {
-  console.log("funkies", JSON.stringify(firebase.postName({ name: "test" })));
-};
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import firebase from '../../firebaseConfig';
 
 export default class MeetupCreate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createEvent = this.createEvent.bind(this);
+    this.state = {
+      event: {
+        id: '123',
+        title: 'tittel',
+        attendees: ['222'],
+        location: 'oslo',
+        time: {
+          start: '11',
+          end: '12',
+        },
+        creator: '278',
+        attendeeLimit: -1,
+        tags: ['tag'],
+      },
+    };
+  }
+
+  async createEvent() {
+    const eventData = this.state.event;
+    const eventId = await firebase.postEvent(eventData);
+    console.log('eventId', eventId);
+  }
+
   render() {
     return (
       <View
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#F80303",
-          color: "white"
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#F80303',
+          color: 'white',
         }}
       >
         <Input
           placeholder="INPUT WITH ICON"
-          leftIcon={{ type: "font-awesome", name: "chevron-left" }}
-          onChangeText={createEvent}
+          leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+          onChangeText={this.createEvent}
         />
         <Text>Create a meetup here!</Text>
-        <Button title="Beep" onPress={createEvent} />
+        <Button title="Beep" onPress={this.createEvent} />
       </View>
     );
   }
