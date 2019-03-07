4b544465ec0d5e83adbd771487ec8d91410c859a
tommyno <tommy@netliferesearch.com>
2019-03-07 11:06:54 +0200

    Cleanup pathand name errors

--- App.js
+++ App.js
@@ -6,11 +6,11 @@ import Store from './Store';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 // Import screens
-import HomeScreen from './src/components/layout/Home/home';
-import LeaderboardScreen from './src/components/layout/Leaderboard/leaderboard';
-import MeetupListScreen from './src/components/layout/MeetupList/meetupList';
-import MeetupEventScreen from './src/components/layout/MeetupEvent/meetupEvent';
-import MeetupCreateScreen from './src/components/layout/MeetupCreate/meetupCreate';
+import HomeScreen from './src/screens/HomeScreen';
+import LeaderboardScreen from './src/screens/LeaderboardScreen';
+import MeetupListScreen from './src/screens/MeetupList';
+import MeetupEventScreen from './src/screens/MeetupEvent';
+import MeetupCreateScreen from './src/screens/MeetupCreate';
 
 const RootStack = createStackNavigator(
   {
