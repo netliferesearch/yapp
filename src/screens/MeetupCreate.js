b91be6b3d09a22476669b7c983b8bf10d5042ee9
Haakon Borch <haakon.borch@netlife.com>
2019-03-07 16:01:57 +0200

    :sparkles: add firebaseconfig with helpermethod-export

--- src/screens/MeetupCreate.js
+++ src/screens/MeetupCreate.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input } from "react-native-elements";
+import firebase from "../../firebaseConfig";
+
+const createEvent = () => {
+  console.log("funkies", JSON.stringify(firebase.postName({ name: "test" })));
+};
 
 export default class MeetupCreate extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F80303', color: 'white'}}>
-          <Text>Create a meetup here!</Text>
-          <Button
-            title="Go to the Homescreen"
-            onPress={() => this.props.navigation.navigate('Home')}
-          />
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#F80303",
+          color: "white"
+        }}
+      >
+        <Input
+          placeholder="INPUT WITH ICON"
+          leftIcon={{ type: "font-awesome", name: "chevron-left" }}
+          onChangeText={createEvent}
+        />
+        <Text>Create a meetup here!</Text>
+        <Button title="Beep" onPress={createEvent} />
+      </View>
+    );
   }
+}
