0533dd52d6715597b337e9f734df50f1ff95d368
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-03-07 16:56:04 +0200

    Change path to logo file

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Image } from "react-native";
-import * as yappLogo from '../../../assets/logo.svg';
+import * as yappLogo from '../../assets/logo';
 
 export default class Logo extends React.Component {
     render() {
