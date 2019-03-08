2d0ff141e33c1daf858ab6f1965a45452e945c72
Haakon Borch <haakon.borch@netlife.com>
2019-03-08 10:25:10 +0200

    :sparkles: add function for getting all events and locations

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,47 +1,60 @@
-import * as firebase from 'firebase';
-import uuid from 'uuid/v4';
+import * as firebase from "firebase";
+import uuid from "uuid/v4";
 
 var config = {
-  apiKey: 'AIzaSyBvVYdjcVSyJ3_HdA54XWTjZGqmKbmj8tw',
-  authDomain: 'yapp-70773.firebaseapp.com',
-  databaseURL: 'https://yapp-70773.firebaseio.com',
-  projectId: 'yapp-70773',
-  storageBucket: 'yapp-70773.appspot.com',
-  messagingSenderId: '128914629238',
+  apiKey: "AIzaSyBvVYdjcVSyJ3_HdA54XWTjZGqmKbmj8tw",
+  authDomain: "yapp-70773.firebaseapp.com",
+  databaseURL: "https://yapp-70773.firebaseio.com",
+  projectId: "yapp-70773",
+  storageBucket: "yapp-70773.appspot.com",
+  messagingSenderId: "128914629238"
 };
 firebase.initializeApp(config);
 export default {
   postEvent: function(eventData) {
     const eventId = uuid();
-    console.log('firebase eventdata', eventData);
+    console.log("firebase eventdata", eventData);
     firebase
       .database()
-      .ref('events/' + eventId)
+      .ref("events/" + eventId)
       .set(eventData);
     return eventId;
   },
-  getName: function(name) {
-    console.log('GET NAME ', name);
+  getEvent: function(name) {
+    console.log("GET NAME ", name);
     return firebase
       .database()
-      .ref('users/' + name)
-      .once('value')
+      .ref("events/" + name)
+      .once("value")
       .then(function(snapshot) {
-        var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        var username =
+          (snapshot.val() && snapshot.val().username) || "Anonymous";
         console.log(snapshot.val());
         return username;
       });
   },
-  getAll: function() {
-    console.log('GET ALL');
+  getAllEvents: function() {
+    console.log("GET ALL Events");
     return firebase
       .database()
-      .ref('users/')
-      .once('value')
+      .ref("events/")
+      .once("value")
       .then(function(snapshot) {
         // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
         // console.log(snapshot.val());
         return snapshot.val();
       });
   },
+  getAllLocations: function() {
+    console.log("GET ALL Locations");
+    return firebase
+      .database()
+      .ref("locations/")
+      .once("value")
+      .then(function(snapshot) {
+        // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        // console.log(snapshot.val());
+        return snapshot.val();
+      });
+  }
 };
