d24c3f662e823f62dee1fdccfc50f467d0c04cff
Ådne Forslund <adne.forslund@gmail.com>
2019-09-12 16:15:31 +0200

    add groq query that should work

---
+++ src/actions/SponsorsAction.js
@@ -0,0 +1,33 @@
+import { YellowBox } from 'react-native';
+import actionType from '../utils/redux';
+import sanity from '../../sanity';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+
+export default function readSponsors() {
+  const query = `
+        *[_type == 'konferanse' && _id == "1dac660d-d163-4c92-9a3f-b96c2a3dd0c7"][0]{
+
+          "superPartners": sponsors[level == "superpartner"]{_key,level,"sponsor": ref->{..., "image": image.asset->url}},
+          "mainPartners": sponsors[level == "mainpartner"]{_key,level,"sponsor": ref->{..., "image": image.asset->url}},
+          "partners": sponsors[level == "partner"]{_key,level,"sponsor": ref->{..., "image": image.asset->url}},
+        }`;
+  return dispatch => {
+    sanity
+      .fetch(query)
+      .then(sponsors => {
+        console.log('SPONSOR', sponsors);
+        dispatch({
+          type: actionType.sponsorReadFulfilled,
+          payload: sponsors,
+        });
+      })
+      .catch(err => {
+        console.log('Sponsor ERROR', err);
+        dispatch({
+          type: actionType.sponsorReadRejected,
+          error: `Error while reading sponsors. Status message: ${err.message}`,
+        });
+      });
+  };
+}
