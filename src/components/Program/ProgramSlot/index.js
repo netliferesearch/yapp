f6046fd625ba2b24a47fd9e751d6c5de672c3f4f
Kristoffer Brabrand <kristoffer@brabrand.no>
2019-09-13 14:17:25 +0200

    Format dates and take time zones into consideration

--- src/components/Program/ProgramSlot/index.js
+++ src/components/Program/ProgramSlot/index.js
@@ -2,23 +2,23 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import propTypes from 'prop-types';
+import format from 'date-fns/format';
 import apiToValueChecker from '../../../utils/apiToValue';
+
 import styles from './styles';
 
 export const ProgramSlot = props => {
   const { slot } = props;
   const programStart = apiToValueChecker(slot, 'item', 'startTime') ? slot.item.startTime : '';
   const programEnd = apiToValueChecker(slot, 'item', 'endTime') ? slot.item.endTime : null;
-  console.log(programStart);
-  const start = programStart.substring(programStart.indexOf('T') + 1).substring(0, 5);
-  const end = programEnd.substring(programEnd.indexOf('T') + 1).substring(0, 5);
+
+  const start = format(new Date(programStart), 'kk:mm');
+  const end = format(new Date(programEnd), 'kk:mm');
 
   const sessionObject = apiToValueChecker(slot, 'item', 'session') ? slot.item.session : null;
   const programTitle = sessionObject && apiToValueChecker(sessionObject, 'title', 'nb') ? sessionObject.title.nb : null;
-  const programSlug =
-    sessionObject && apiToValueChecker(sessionObject, 'slug', 'nb', 'current') ? sessionObject.slug.nb.current : null;
-  const programTrack =
-    sessionObject && apiToValueChecker(sessionObject, 'track', 'title', 'nb') ? sessionObject.track.title.nb : null;
+  const programSlug = sessionObject && apiToValueChecker(sessionObject, 'slug', 'nb', 'current') ? sessionObject.slug.nb.current : null;
+  const programTrack = sessionObject && apiToValueChecker(sessionObject, 'track', 'title', 'nb') ? sessionObject.track.title.nb : null;
   return (
     <View style={styles.programSlotContainer}>
       <View style={styles.leftColumn}>
