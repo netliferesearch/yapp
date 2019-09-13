222adcf0235821ced2f3e7c8e84362b4b500af2e
Ådne Forslund <adne.forslund@gmail.com>
2019-09-13 13:31:10 +0200

    modify image query for sponsors

--- src/actions/SponsorsAction.js
+++ src/actions/SponsorsAction.js
@@ -8,9 +8,9 @@ export default function readSponsors() {
   const query = `
         *[_type == 'konferanse' && _id == "1dac660d-d163-4c92-9a3f-b96c2a3dd0c7"][0]{
 
-          "superPartners": sponsors[level == "superpartner"]{_key,level,"sponsor": ref->{..., "image": image.asset->url}},
-          "mainPartners": sponsors[level == "mainpartner"]{_key,level,"sponsor": ref->{..., "image": image.asset->url}},
-          "partners": sponsors[level == "partner"]{_key,level,"sponsor": ref->{..., "image": image.asset->url}},
+          "superPartners": sponsors[level == "superpartner"]{_key,level,"sponsor": ref->{..., "image": imageWithColor.asset->url}},
+          "mainPartners": sponsors[level == "mainpartner"]{_key,level,"sponsor": ref->{..., "image": imageWithColor.asset->url}},
+          "partners": sponsors[level == "partner"]{_key,level,"sponsor": ref->{..., "image": imageWithColor.asset->url}},
         }`;
   return dispatch => {
     sanity
