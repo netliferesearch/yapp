ff171028c866df24d544d7ec7726a31962d9e5fb
Tatsumi Suzuki <tatsumi.suzuki@netlife.com>
2019-09-17 09:13:19 +0200

    Resize splash screen. Add custom font to empty program.

--- src/components/Program/EmptyProgram.js
+++ src/components/Program/EmptyProgram.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { theme } from '../../styles/theme';
+import { theme, font } from '../../styles/theme';
 
 export default function EmptyProgram() {
   return (
     <View>
-      <Text style={{ marginBottom: theme.margins.sm }}>
+      <Text style={[{ marginBottom: theme.margins.sm }, font.smRegular]}>
         Add talks to your program by clicking the star next to what you want to attend on the program.
       </Text>
-      <Text>Happy planning, and enjoy Y!</Text>
+      <Text style={font.smRegular}>Happy planning, and enjoy Y!</Text>
     </View>
   );
 }
